76957cf283752e2f58d9b21187a1ac5f3159c8e2
sumonthaa <152372389+sumonthaa@users.noreply.github.com>
2024-02-21 15:42:54 +0700

    Update webboard using bootstrap
    update file index.php, login.php, vetify.php and create file nav.php(Finish work7)

--- index.php
+++ index.php
@@ -6,42 +6,48 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>Webboard Sumontha</title>
 </head>
 <body>
+<div class="container-lg">
     <h1 style="text-align: center;">Webboard Sumontha</h1>
     <hr>
-    <form>
-        หมวดหมู่:
-        <select>
-            <option value="all">--ทั้งหมด--</option>
-            <option value="general">เรื่องทั่วไป</option>
-            <option value="study">เรื่องเรียน</option>
-        </select>
+    <?php include "nav.php"?>
+
+<div class="mt-3 d-flex justify-content-between">
+    <div>
+    <label>หมวดหมู่</label>
+    <span class="dropdown">
+    <button class="btn btn-light btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    --ทั้งหมด--
+    </button>
+    <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">ทั้งหมด</a></li>
+    <li><a class="dropdown-item" href="#">เรื่องเรียน</a></li>
+    <li><a class="dropdown-item" href="#">เรื่องทั่วไป</a></li>
+  </ul>
+</div>
+<?php if(isset($_SESSION['id'])){ ?>
+<div><a href="newpost.php" class="btn btn-success btn-sm">
+    <i class="bi bi-plus"></i> สร้างกระทู้ใหม่</a>
+    </div>
+    <?php } ?>
+    </div>
+
+    <table class="table table-striped mt-4 ">
         <?php
-        if(!isset($_SESSION['id'])){
-            echo "<a href=login.php style='float:right'; >เข้าสู่ระบบ</a>";
-        }else{
-            echo "<div style='float:right'>
-                ผู้ใช้งานระบบ : $_SESSION[username]&nbsp;&nbsp;
-                <a href=logout.php >ออกจากระบบ</a>
-                </div><br>
-                <a href=newpost.php>สร้างกระทู้ใหม่</a>
-                ";    
-        }
-        ?>
-    </form>
-    <ul>
-    <?php
-            for($i=1;$i<=10;$i++){
-                echo"<li>";
-                echo "<a href=post.php?id=$i>กระทู้ที่ $i</a>";
+            for($i=1;$i<=10;$i++){       
+                echo "<tr><td class='d-flex justify-content-between'><a href=post.php?id=$i sytle=text-decoration:none>กระทู้ที่ $i</a>";
                 if(isset($_SESSION['id']) && $_SESSION['role']=='a'){
-                    echo "&nbsp;&nbsp;&nbsp;<a href=delete.php?id=$i>ลบ</a>";
+                    echo "&nbsp;&nbsp;&nbsp;<a href=delete.php?id=$i class='btn btn-danger btn-sm'><i class='bi bi-trash'></i></a>";
                 }
-                echo "</li>";
+                echo "</td></tr>";
             }
         ?>
-    </ul>
+    </table>
+    </div>
 </body>
 </html>
--- verify.php
+++ verify.php
@@ -13,23 +13,32 @@
     <title>verify</title>
 </head>
 <body>
-    <h1 style="text-align: center;">Webboard Sumontha</h1>
-    <hr>
-    <div style="text-align: center;">
+
+    
     <?php
         if (($_POST["login"] == "admin") && ($_POST["pwd"] == "ad1234")) {
             $_SESSION["username"]="admin";
             $_SESSION["role"]="a";
             $_SESSION["id"]=session_id();
-            echo "ยินดีต้อนรับคุณ ADMIN";
+            header("location:index.php");
+            die();
+            //echo "ยินดีต้อนรับคุณ ADMIN";
         } else if (($_POST["login"] == "member") && ($_POST["pwd"] == "mem1234")) {
             $_SESSION["username"]="member";
             $_SESSION["role"]="m";
             $_SESSION["id"]=session_id();
-            echo "ยินดีต้อนรับคุณ MEMBER";
-        } else {echo "ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง";}
+            header("location:index.php");
+            die();
+            //echo "ยินดีต้อนรับคุณ MEMBER";
+        }
+         else {
+            $_SESSION['error']='error';
+            header("location:login.php");
+            die();
+            //echo "ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง";
+        }
     ?><br>
       <a href="index.php">กลับไปยังหน้าหลัก</a>
-    </div>
+    
 </body>
 </html>

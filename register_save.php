9298f5b61e973f8c38316f81424f667314bb2dd8
sumonthaa <152372389+sumonthaa@users.noreply.github.com>
2024-04-01 14:34:04 +0700

    up
    up

---
+++ register_save.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+$login=$_POST['login'];
+$passwd=sha1($_POST['pwd']);
+$name=$_POST['name'];
+$gender=$_POST['gender'];
+$email=$_POST['email'];
+
+$conn=new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
+$sql="SELECT * FROM user where login='$login'";
+    $result=$conn->query($sql);
+    if($result->rowCount()==1){
+        $_SESSION['add_login']='error';    
+    }else{
+        $sql = "INSERT INTO user (login, password, name, gender, email, role) 
+            VALUES ('$login', '$passwd', '$name', '$gender', '$email', 'm')";
+        $conn->exec($sql);
+        $_SESSION['add_login']='success';
+    }    
+    $conn = null;
+    header("location:register.php");
+    die();
+?>
